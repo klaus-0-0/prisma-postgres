8ca045ec9002f3b0cfd2f7904eec33ab362024c2
Jin kazama <klaus-0-0>
2024-12-08 20:38:23 +0530

    i dont know

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
 
-const postRoutes = require('./posts');
-
-// Use CORS middleware
+// Middleware
 app.use(cors());
-app.use(express.json());  // Middleware to parse JSON bodies
+app.use(express.json());
 
-// Define route for posts
-app.use('/api/posts', postRoutes);
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
 });
- 
