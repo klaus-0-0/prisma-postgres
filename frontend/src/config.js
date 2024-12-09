de0e231d8505609d8db8a9b0c720d37363dcd207
Jin kazama <klaus-0-0>
2024-12-09 16:20:17 +0530

    deploy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,8 @@
 // src/config.js
 const config = {
 //  apiUrl: 'http://localhost:8080/api'
-  apiUrl: 'https://deploy-5-pot3.onrender.com/api'
+//  apiUrl: 'https://deploy-5-pot3.onrender.com/api'
+  apiUrl: 'https://deploy-hmbw.onrender.com/api'
 };
 
 export default config;
