218f99463315da7dcad54cd903e87b1de25904df
Jin kazama <klaus-0-0>
2024-12-09 16:47:19 +0530

    deploy

--- backend/prisma/migrations/20241209105829_init/migration.sql
+++ backend/prisma/migrations/20241209105829_init/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
-CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "topic" TEXT NOT NULL,
-    "published" BOOLEAN NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+CREATE TABLE public."Post" (
+  "id" SERIAL PRIMARY KEY,
+  "title" TEXT NOT NULL,
+  "content" TEXT NOT NULL,
+  "topic" TEXT NOT NULL,
+  "published" BOOLEAN DEFAULT false,
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
