be640cc666e026c195d9e52d6389245245874027
Jin kazama <klaus-0-0>
2024-12-09 16:28:58 +0530

    deploy

--- backend/prisma/migrations/20241208095304_adjusted_schema/migration.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "Post" (
-  "id" SERIAL PRIMARY KEY,
-  "title" TEXT NOT NULL,
-  "content" TEXT NOT NULL,
-  "topic" TEXT NOT NULL,
-  "published" BOOLEAN DEFAULT false,
-  "createdAt" TIMESTAMP DEFAULT NOW(),
-  "updatedAt" TIMESTAMP
-);
