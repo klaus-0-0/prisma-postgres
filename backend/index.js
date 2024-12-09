2a56361211abe4c4db0455934d9f31ba9ab5aab3
Jin kazama <klaus-0-0>
2024-12-09 14:25:58 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config(); // Load environment variables
+require('dotenv').config();
 
-const postRoutes = require('./posts'); // Import the router
+const postRoutes = require('./routes/posts');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Middleware
-app.use(cors());
-app.use(express.json()); // Parse JSON bodies
+app.use(cors({
+  origin: 'https://your-frontend-domain.com',
+  credentials: true
+}));
+app.use(express.json());
 
-// Use the post routes
-app.use('/api/posts', postRoutes); // Prefix API routes with /api/posts
+// Routes
+app.use('/api/posts', postRoutes);
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 // src/config.js
 const config = {
+//  apiUrl: 'http://localhost:8080/api'
   apiUrl: 'https://deploy-5-pot3.onrender.com/api'
 };
 
