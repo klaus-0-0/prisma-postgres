fd282fa5a13e4e192c12467ef924df088e437612
Jin kazama <klaus-0-0>
2024-12-12 03:09:15 +0530

    deploy

--- backend/prisma/migrations/20241211213737_init/migration.sql
+++ backend/prisma/migrations/20241211213737_init/migration.sql
@@ -13,13 +13,17 @@ CREATE TABLE "Post" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "topic" TEXT NOT NULL,
-    "published" BOOLEAN NOT NULL,
+    "topic" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" INTEGER,
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
