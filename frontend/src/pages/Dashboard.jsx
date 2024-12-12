56d4f5c542418b4847a50e89ccd7c33d041827f9
Jin kazama <klaus-0-0>
2024-12-12 16:17:04 +0530

    deploy

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,62 @@
-const Dashboard = ()=> {
-    return <div>
-        <label>hii</label>
-    </div>
-}
+import { useState } from 'react';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import Home from '../pages/Home';
+import TopicPage from '../pages/TopicPage';
+import '../index.css';
+import buttonSvg from '../assets/button.svg';
+import daySvg from '../assets/day.svg';
+import nightSvg from '../assets/night.svg';
 
-export default Dashboard
+const Dashboard = ({ topics, isAuthenticated }) => { // Accept isAuthenticated as a prop
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  };
+
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Navbar />
+      <button className="toggle-button" onClick={toggleSidebar}>
+        <img src={buttonSvg} alt="Toggle Sidebar" />
+      </button>
+      <button className="theme-switch-button" onClick={toggleTheme}>
+        <img src={darkMode ? daySvg : nightSvg} alt="Toggle Theme" className="theme-icon" />
+      </button>
+      {showSidebar && (
+        <div className="sidebar-container">
+          <a href="/" className="home-button">Home</a>
+          <Sidebar topics={topics} />
+        </div>
+      )}
+      <div className={`main-content ${showSidebar ? 'sidebar-visible' : ''}`}>
+        <Routes>
+          <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
+          {topics.map((topic, index) => (
+            <Route
+              key={index}
+              path={`/topics/${topic.toLowerCase()}`}
+              element={<TopicPage topic={topic} />}
+            />
+          ))}
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
